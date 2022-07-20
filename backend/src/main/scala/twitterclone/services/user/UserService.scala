c3877a52880d50ef17cec098d75cf3d07fb4d97e
Sophie Collard <sophie.collard@hotmail.fr>
2022-07-20 21:10:19 +0100

    Define and partially implement user service

---
+++ backend/src/main/scala/twitterclone/services/user/UserService.scala
@@ -0,0 +1,60 @@
+package twitterclone.services.user
+
+import cats.implicits._
+import cats.{Monad, ~>}
+import twitterclone.model.Id
+import twitterclone.model.user.{Handle, Name, User}
+import twitterclone.repositories.user.UserRepository
+import twitterclone.services.error.ServiceError.{resourceNotFound, userHandleNotFound}
+import twitterclone.services.error.{ServiceError, ServiceErrorOr}
+import twitterclone.services.syntax._
+
+trait UserService[F[_]] {
+
+  /** Creates a new user */
+  def create(handle: Handle, name: Name): F[ServiceErrorOr[User]]
+
+  /** Fetches a user */
+  def get(id: Id[User]): F[ServiceErrorOr[User]]
+
+  /** Fetches a list of users */
+  def getMany(ids: List[Id[User]]): F[ServiceErrorOr[List[User]]]
+
+  /** Fetches a user by its handle */
+  def getByHandle(handle: Handle): F[ServiceErrorOr[User]]
+
+}
+
+object UserService {
+
+  def create[F[_], G[_]: Monad](
+    userRepository: UserRepository[G]
+  )(implicit transactor: G ~> F): UserService[F] =
+    new UserService[F] {
+      /** Creates a new user */
+      override def create(handle: Handle, name: Name): F[ServiceErrorOr[User]] =
+        ??? // TODO implement
+
+      /** Fetches a user */
+      override def get(id: Id[User]): F[ServiceErrorOr[User]] =
+        userRepository.get(id).map {
+          case Some(user) => Right(user)
+          case None       => Left(resourceNotFound(id, "User"))
+        }.transact
+
+      /** Fetches a list of users */
+      override def getMany(ids: List[Id[User]]): F[ServiceErrorOr[List[User]]] =
+        userRepository
+          .getMany(ids)
+          .map(_.asRight[ServiceError])
+          .transact // TODO Fix implementation
+
+      /** Fetches a user by its handle */
+      override def getByHandle(handle: Handle): F[ServiceErrorOr[User]] =
+        userRepository.getByHandle(handle).map {
+          case Some(user) => Right(user)
+          case None       => Left(userHandleNotFound(handle))
+        }.transact
+    }
+
+}
