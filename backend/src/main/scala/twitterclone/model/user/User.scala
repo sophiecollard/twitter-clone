354af65105d89c243ed9127891c7690161f3dec9
Sophie Collard <sophie.collard@hotmail.fr>
2022-07-20 21:09:47 +0100

    Introduce user status

--- backend/src/main/scala/twitterclone/model/user/User.scala
+++ backend/src/main/scala/twitterclone/model/user/User.scala
@@ -2,4 +2,4 @@ package twitterclone.model.user
 
 import twitterclone.model.Id
 
-final case class User(id: Id[User], handle: Handle, name: Name)
+final case class User(id: Id[User], handle: Handle, name: Name, status: Status)
