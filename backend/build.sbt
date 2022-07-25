1bcc689c24574d40ef769ad2c3748f2db6c3e995
Sophie Collard <sophie.collard@hotmail.fr>
2022-07-25 21:26:03 +0100

    Tiny refactor of dependencies

--- backend/build.sbt
+++ backend/build.sbt
@@ -1,5 +1,3 @@
-import Dependencies._
-
 ThisBuild / scalaVersion     := "2.13.8"
 ThisBuild / version          := "0.1.0-SNAPSHOT"
 ThisBuild / organization     := "com.github.sophiecollard"
@@ -8,7 +6,7 @@ ThisBuild / organizationName := "SophieCollard"
 lazy val root = (project in file("."))
   .settings(
     name := "twitter-clone",
-    libraryDependencies ++= asSeq,
+    libraryDependencies ++= Dependencies.list,
     resolvers += "Sonatype OSS Releases" at "https://oss.sonatype.org/content/repositories/releases/",
     scalacOptions ++= Seq(
       "-deprecation",
--- backend/project/Dependencies.scala
+++ backend/project/Dependencies.scala
@@ -11,43 +11,43 @@ object Dependencies {
   private val http4sJettyServerVersion = "0.23.10"
   private val scalaTestVersion = "3.2.11"
 
-  private val cats = Seq(
+  private val cats = List(
     "org.typelevel" %% "cats-core"
   ).map(_ % catsVersion)
 
-  private val catsEffect = Seq(
+  private val catsEffect = List(
     "org.typelevel" %% "cats-effect"
   ).map(_ % catsEffectVersion)
 
-  private val circe = Seq(
+  private val circe = List(
     "io.circe" %% "circe-core",
     "io.circe" %% "circe-generic"
   ).map(_ % circeVersion)
 
-  private val ciris = Seq(
+  private val ciris = List(
     "is.cir" %% "ciris"
   ).map(_ % cirisVersion)
 
-  private val doobie = Seq(
+  private val doobie = List(
     "org.tpolecat" %% "doobie-core"
   ).map(_ % doobieVersion)
 
-  private val http4s = Seq(
+  private val http4s = List(
     "org.http4s" %% "http4s-circe",
     "org.http4s" %% "http4s-core",
     "org.http4s" %% "http4s-dsl",
     "org.http4s" %% "http4s-server"
   ).map(_ % http4sVersion)
 
-  private val http4sJettyServer = Seq(
+  private val http4sJettyServer = List(
     "org.http4s" % "http4s-jetty-server_2.13"
   ).map(_ % http4sJettyServerVersion)
 
-  private val scalaTest = Seq(
+  private val scalaTest = List(
     "org.scalatest" %% "scalatest"
   ).map( _ % scalaTestVersion % Test)
 
-  val asSeq: Seq[ModuleID] =
+  val list: List[ModuleID] =
     cats ++
       catsEffect ++
       circe ++
